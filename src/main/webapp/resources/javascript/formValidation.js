640a70995f36ac9bf4e77e2294b1508c53de36cf
Yixue04 <vivien.hou@yahoo.de>
2022-02-03 22:59:44 +0100

    Separation userProfile and updateProfile + change style and validation

--- src/main/webapp/resources/javascript/formValidation.js
+++ src/main/webapp/resources/javascript/formValidation.js
@@ -3,12 +3,13 @@ var mail = $("#email").value();
 var mailR = $("#emailRepeat").value();
 
 var checkMail = $("#checkMail");
-if(mail === mailR) {
-    checkMail.style.color = "green";
-    checkMail.innerHTML = "";
-} else {
+while (mail !== mailR) {
     checkMail.style.color = "red";
     checkMail.innerHTML = "Die Eingaben stimmen nicht überein.";
+}
+while (mail === mailR) {
+    checkMail.style.color = "green";
+    checkMail.innerHTML = "";
 }
 
 //Check consensus of passwords
@@ -16,10 +17,11 @@ var pswd = $("#password").value();
 var pswdR = $("#passwordRepeat").value();
 
 var checkPswd = $("#checkPswd");
-if(pswd===pswdR) {
-    checkPswd.style.color = "green";
-    checkPswd.innerHTML = "";
-} else {
+while (pswd !== pswdR) {
     checkPswd.style.color = "red";
     checkPswd.innerHTML = "Die Eingaben stimmen nicht überein.";
+}
+while (pswd === pswdR) {
+    checkPswd.style.color = "green";
+    checkPswd.innerHTML = "";
 }
