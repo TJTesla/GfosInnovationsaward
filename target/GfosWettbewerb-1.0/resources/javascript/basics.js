fe25dd6776b5d282c0ff88238b457c22b9d15c6e
Yixue04 <vivien.hou@yahoo.de>
2022-02-09 11:40:11 +0100

    Save jobOverview and offer

--- target/GfosWettbewerb-1.0/resources/javascript/basics.js
+++ target/GfosWettbewerb-1.0/resources/javascript/basics.js
@@ -8,3 +8,9 @@ function closeNav() {
     document.getElementById("sideNav").style.width = "0";
     $("#sideNav").hide(500, "linear");
 }
+
+function redirect(url) {
+    var pageUrl = $(location).attr("href");
+    url = pageUrl.toString() + url.toString();
+    window.location.href = url;
+}
